e45ab631c62262fae30c77a39867d5e4c03f34d4
Mart Pluijmaekers <mart@gewis.nl>
2015-02-11 23:03:56 +0100

    Some changes in the graph and added the graph parser

---
+++ Assignment 1/code/parseDependencies.php
@@ -0,0 +1,23 @@
+<?php
+
+$dotfile = file_get_contents($argv[1]);
+
+preg_match_all("/([A-Z]+[0-9]+ -> [A-Z]+[0-9]+) \#(.*)/", $dotfile, $matches);
+
+echo '\begin{longtable}{lp{11cm}}
+';
+echo '\textbf{Dependency} & \textbf{Reason} \\\\
+';
+for($i = 0; $i < count($matches[0]); $i++){
+    $req = $matches[1][$i];
+    $remark = $matches[2][$i];
+
+    $req = str_replace(" -> ", "$\\rightarrow$", $req);
+
+    echo "{\\sc $req} & $remark. \\\\ \n";
+}
+
+echo '\end{longtable}'
+// print_r($matches);
+
+?>
