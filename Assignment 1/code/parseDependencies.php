1d7ee256bf42ef368ca80bba3d501a18ff3edea1
Mart Pluijmaekers <mart@gewis.nl>
2015-02-12 00:00:57 +0100

    Final push for assignment 1

--- Assignment 1/code/parseDependencies.php
+++ Assignment 1/code/parseDependencies.php
@@ -6,13 +6,13 @@ preg_match_all("/([A-Z]+[0-9]+ -> [A-Z]+[0-9]+) \#(.*)/", $dotfile, $matches);
 
 echo '\begin{longtable}{lp{11cm}}
 ';
-echo '\textbf{Dependency} & \textbf{Reason} \\\\
+echo '\textbf{Dependency} & \textbf{Reason} \\\\  \endhead
 ';
 for($i = 0; $i < count($matches[0]); $i++){
     $req = $matches[1][$i];
     $remark = $matches[2][$i];
 
-    $req = str_replace(" -> ", "$\\rightarrow$", $req);
+    $req = str_replace(" -> ", " $\\rightarrow$ ", $req);
 
     echo "{\\sc $req} & $remark. \\\\ \n";
 }
