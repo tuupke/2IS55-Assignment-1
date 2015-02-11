f107e4c62d9b7ad0832e988c7a59aa02cfc8b331
Mart Pluijmaekers <mart@gewis.nl>
2015-02-11 22:50:27 +0100

    Pushing progress, sorry mr Serebrenik

--- Assignment 1/code/analyze.php
+++ Assignment 1/code/analyze.php
@@ -34,10 +34,12 @@ $contents = file_get_contents(dirname(__FILE__)."/requirements.txt");
 $contents = explode("\n", $contents);
 
 $keywords = array(
-    "optionality" => array("possibly","eventually","if case","if possible","if appropriate","if needed", "additionally", "and in a more", "there is a need","maybe"),
-    "vagueness" => array("clear","easy","strong","good","bad","useful","significant","adequate","recent","praised", "goals", "strict or lenient", "choice","should be consistent to",'"forgiving"',"small","plausible","large","provide hints","complex errors"),
-    "underspecification" => array("data flow","control flow","write access","remote access","authorized","access","testing","functional testing","structural testing","unit testing"),
+    "optionality" => array("possibly","eventually","if case","if possible","if appropriate","if needed", "maybe","and/or", "option"),
+    "vagueness" => array("clear","easy","strong","good","bad","useful","significant","adequate","recent","praised", "goals", "strict or lenient", "choice","should be consistent to",'"forgiving"',"small","plausible","large","complex errors","intuitive","and in a way","accurately"),
+    "underspecification" => array("data flow","control flow","write access","remote access","authorized","access","testing","functional testing","structural testing","unit testing","and in a way","teaching preferences", "on-line support"),
 );
+
+if(in_array("analyze",$argv)){
 
 echo "
 The results of the analysis are:
@@ -63,12 +65,49 @@ foreach($contents as $ur){
 echo "\\end{longtable}
 
 ";
+}
 
+if(in_array("report",$argv)){
+    $output = array();
+
+    foreach($keywords as $k => $v){
+        $output[$k] = "";
+    }
+
+
+    foreach($contents as $ur){
+        $split = '/^([A-Z]+[0-9]+) (.*$)/i';
+        $replacement = '\\texttt{$1} & $2';
+        preg_match($split, $ur, $spl);
+        // print_r($spl);
+        $tempLine = prepForAnalysis($spl[2]);
+
+        $analysis = calcScores($keywords, $tempLine);
+        foreach($analysis as $k => $v){
+            if($v != 0){
+                $output[$k] .= '\item['.$spl[1]."] a\n";
+            }
+        }
+    }
+
+    foreach($output as $k => $v){
+        echo "The user requirements that match for {\\sc $k} are:
+\\begin{itemize}
+".$v."
+\\end{itemize}
+
+";
+    }
+    // print_r($output);
+}
+
+if(in_array("keywords", $argv)){
 foreach($keywords as $key => $val){
     echo "The keywords used for {\\sc $key} are:
-\\begin{enumerate}
+\\begin{itemize} \\itemsep0em
 \\item ".implode("\n \\item ",$val)."
-\\end{enumerate}";
+\\end{itemize}";
+}
 }
 
 ?>
